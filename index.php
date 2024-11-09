b77686a2fd1274251afe96d60d89474cff117f0f
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-11-09 16:33:07 -0500

    first

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <div class="tuff ">
+        <img class="img" src="./images/bg-main-mobile.png" alt="colorful background image">
+    </div>
+    <div>
+        <img src="./images/bg-card-back.png" class="frontCard" alt="image of fake card">
+        <img src="./images/bg-card-front.png" class="backCard" alt="image of fake card">
+    </div>
+</body>
+</html>
